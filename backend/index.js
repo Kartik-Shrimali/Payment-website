57a6affa35e8e0e3fbbb1ce7aeffdad79abc971c
Kartik Shrimali <kartikshrimali62@gmail.com>
2025-02-03 00:36:08 +0530

    Downloaded jwt , added cors and added JWT_SECRET

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const apiRouter = require('./routes/index');
+const cors = require('cors');
+
 const app = express();
 app.use('./api/v1' , apiRouter);
+app.use(cors());
+app.use(express.json());
 
 
 app.listen(3000);
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const Router = express.Router();
-
 const UserRouter = require("./user");
+
 Router.use("./users" , UserRouter);
 
 module.exports = {
