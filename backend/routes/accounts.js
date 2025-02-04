08cfca6fb19627652e50300b71d1b73bc81bbee7
Kartik Shrimali <kartikshrimali62@gmail.com>
2025-02-04 20:05:06 +0530

    Completed transaction route

--- backend/routes/accounts.js
+++ backend/routes/accounts.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.router();
-const authMiddleware = require("../middleware")
+const authMiddleware = require("../middleware");
+const Account = require("../db")
 
 router.get('/balance', authMiddleware, async (req, res) => {
     let id = req.body.userId;
@@ -23,6 +24,49 @@ router.get('/balance', authMiddleware, async (req, res) => {
 })
 
 router.post('/transfer', authMiddleware, async (req, res) => {
+    const session = await mongoose.startSession();
+
+    session.startTransaction();
+
+    const ReceiverId = req.body.id;
+    const amount = req.body.amount;
+    const Userid = req.body.Userid;
+
+    const account = await Account.findOne({Userid : Userid }).session(session);
+
+    if(!account){
+        await session.abortTransaction();
+        return res.json({
+            msg : "Your account does not exist !"
+        })
+    }
+
+    if(account.balance < amount)
+    {
+        await session.abortTransaction();
+        return res.json({
+            msg : "You have insufficient balance!!!"
+        })
+    }
+
+    const Receiver = await Account.findOne({
+        Userid : ReceiverId
+    }).session(session);
+
+    if(!Receiver){
+        await session.abortTransaction();
+        return res.json({
+            msg : "The person you are trying to send money doesm't exist !!"
+        })
+    }
+
+    await Account.updateOne({ Userid : Userid} , {$inc : {balance : -amount}} , {session});
+    await Account.updateOne({ Userid : ReceiverId} , {$inc : {balance : amount}} , {session});
+
+    await session.commitTransaction();
+    res.json({
+        msg : "Transfer Successful",
+    })
 
 })
 
